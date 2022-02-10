2381f2485022560f3d100f998a734b7062edfdab
Ingvar <example@mail.com>
2022-02-10 22:52:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+console.log("arr" + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+incr++; //оператор инкремента, увеличения на 1
+decr--; //оператор дикримента, уменьшения на 1
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2*4 === "8"); //сравнивание, является ли 2*4=8. == сравнение, а === строгое сравнение, а = это прсваивание
+
+&& // оператор И
+|| // оператор или
+
+const isChecked = false;
+        isClose = false;
+console.log(isChecked || !isClose); // оператор ! (отрицания) меняет true на false
+
+
